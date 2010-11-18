528cdefc3f9a12e4b586741f5a72ef52c2075443
Andrew Main (Zefram) <zefram@fysh.org>
2010-11-18 20:12:24 +0000

    Import of ZEFRAM/Time-OlsonTZ-Data-0.201015 from CPAN.
    gitpan-cpan-distribution: Time-OlsonTZ-Data
    gitpan-cpan-version:      0.201015
    gitpan-cpan-path:         ZEFRAM/Time-OlsonTZ-Data-0.201015.tar.gz
    gitpan-cpan-author:       ZEFRAM
    gitpan-cpan-maturity:     released

--- lib/Time/OlsonTZ/Data.pm
+++ lib/Time/OlsonTZ/Data.pm
@@ -42,7 +42,7 @@ use strict;
 use Carp qw(croak);
 use File::Spec ();
 
-our $VERSION = "0.201014";
+our $VERSION = "0.201015";
 
 use parent "Exporter";
 our @EXPORT_OK = qw(
@@ -68,7 +68,7 @@ retain this format in the future.
 
 =cut
 
-use constant olson_version => "2010n";
+use constant olson_version => "2010o";
 
 =back
 
